70cd41e28b2ddbc029759f8313a16a2e15927799
MMcClure11 <mcclme02@luther.edu>
2020-11-23 13:41:35 -0600

    action for getMessages sense data to redux store

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -3,6 +3,9 @@ export function getMessages() {
   return (dispatch) => {
     fetch('http://localhost:3000/messages')
     .then(resp => resp.json())
-    .then(console.log)
+    .then(messages => dispatch({
+      type: 'GET_MESSAGES',
+      messages
+    }))
   }
 }
