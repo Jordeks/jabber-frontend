c399a7eec0ffb79eac7cf709d396df0a39a87004
jordles113 <jordan.t.romero@gmail.com>
2020-11-24 16:17:01 -0600

    adds styling for messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -12,13 +12,26 @@ class Message extends Component {
     render() {
         return (
             <li className='message-list__li'>
-                <span>{this.props.username}:</span> <span>{this.props.content}</span>
-                <span><a href="#popup">Edit</a></span>
-                <span><button className='btn btn--delete' onClick={this.onClick}>
-                    <svg className="icon--trash">
-                            <use href={sprite + '#icon-trash'} />
-                    </svg>
-                </button></span>
+                <div className='message'>
+                    <span className='message__username'>{this.props.username}</span> 
+                    <p className='message__content'>{this.props.content}
+                    {/*<span className='btn btn--delete'><a href="#popup">Edit</a></span>*/}
+                    <span className='btn btn--delete'>
+                        <a href="#popup">
+                            <svg className="icon--trash">
+                                <use href={sprite + '#icon-edit'} />
+                            </svg>
+                        </a>
+                    </span>
+                    <span>
+                        <button className='btn btn--delete' onClick={this.onClick}>
+                            <svg className="icon--trash">
+                                    <use href={sprite + '#icon-trash'} />
+                            </svg>
+                        </button>
+                    </span>
+                    </p>
+                </div>
             </li>
         )
     }
