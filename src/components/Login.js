362385d3cfe553d9eb9e8dc9132dcf6fb59de8c8
MMcClure11 <mcclme02@luther.edu>
2020-11-28 15:20:36 -0600

    creates controlled form for login with local state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+
+  state = {
+    username: '',
+    password: ''
+}
+
+onChange = e => {
+    const { name, value } = e.target
+    this.setState({[name]: value})
+}
+
+onSubmit = e => {
+    e.preventDefault()
+    alert('am submitted')
+    // this.props.login(this.state)
+    // this.setState({
+    //     username: '',
+    //     password: ''
+    // })
+}
+
   render() {
+    console.log(this.state)
     return (
       <div>
-        Login Component
+        <form onSubmit={this.onSubmit}>
+          <input placeholder="username" value={this.state.username} name="username" type="text" onChange={this.onChange} />
+          <input placeholder="password" value={this.state.password} name="password" type="password" onChange={this.onChange} />
+          <input type="submit" value="Log In"/>
+        </form>
       </div>
     )
   }
