66d5b450c58c94e792d548fe9245eae0fee48a47
MMcClure11 <mcclme02@luther.edu>
2020-11-29 10:21:17 -0600

    adds signup action creator

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -62,3 +62,29 @@ export function logout() {
     })
   }
 }
+
+export function signup(credentials, history) {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/signup", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch({
+            type: SET_CURRENT_USER, 
+            user: response
+          })
+          history.push('/messages')
+        }
+      })
+      .catch(console.log)
+  }
+}
